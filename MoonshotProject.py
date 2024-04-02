690453b4aca3252afe474b4d812f88a5fe6ba8f3
Ayan Thomas Chaturvedi <ayan@ayans-mbp.lan>
2024-04-01 21:00:49 -0700

    Adds newly formatted table This update adds a table in exchange for the other 3 labels. It also adds a picture and removes the need to change the lists into normal lists and then back into pickled files.

--- MoonshotProject.py
+++ MoonshotProject.py
@@ -1,41 +1,47 @@
 from tkinter import*
+from tkinter import ttk
+from PIL import Image, ImageTk
 import pickle
 import os
-import time
+from random import choice
+def resized_label(filename="", width=215, height=215, relx=.5, rely=.25, anchor=CENTER):
+    global root
+    image = Image.open(filename)
+    resize_image = image.resize((width, height))
+    img = ImageTk.PhotoImage(resize_image)
+    label1 = Label(root, image=img)
+    label1.image = img
+    label1.pack()
+    label1.place(relx=relx, rely=rely, anchor=anchor)
+    return label1
 nameList=pickle.load(open("name.dat","rb"))
 activityList=pickle.load(open("activity.dat","rb"))
 timeList=pickle.load(open("time.dat","rb"))
 os.system("clear")
 root=Tk()
-root.geometry("500x600")
+root.geometry("700x800")
 root.title("Activity List")
 root.config(bg="CadetBlue1")
-title=Label(root,text="Activity List",font=("times",15,"bold"),bg="CadetBlue1")
+checklist=resized_label("checklist.png",215,215,0.5,0.23)
+title=Label(root,text="Activity List",font=("times",15,"bold"),bg="CadetBlue1",fg="black")
 title.place(relx=0.5,rely=0.05,anchor=CENTER)
-label4=Label(root,text="Name:",font=("times",15),bg="CadetBlue1")
-label4.place(relx=0.3,rely=0.7,anchor=CENTER)
-labelName=Label(root,text="",font=("times",15,"bold"),bg="CadetBlue1")
-labelName.place(relx=0.7,rely=0.7,anchor=CENTER)
-label5=Label(root,text="Activity:",font=("times",15),bg="CadetBlue1")
-label5.place(relx=0.3,rely=0.75,anchor=CENTER)
-labelActivity=Label(root,text="",font=("times",15,"bold"),bg="CadetBlue1")
-labelActivity.place(relx=0.7,rely=0.75,anchor=CENTER)
-label6=Label(root,text="Time:",font=("times",15),bg="CadetBlue1")
-label6.place(relx=0.3,rely=0.8,anchor=CENTER)
-labelTime=Label(root,text="",font=("times",15,"bold"),bg="CadetBlue1")
-labelTime.place(relx=0.7,rely=0.8,anchor=CENTER)
-label1=Label(root,text="Name:",font=("times",15),bg="CadetBlue1")
+label1=Label(root,text="Name:",font=("times",15),bg="CadetBlue1",fg="black")
 label1.place(relx=0.3,rely=0.4,anchor=CENTER)
-nameInput=Entry(root,font=("times",12),bg="CadetBlue1")
+nameInput=Entry(root,font=("times",12),bg="CadetBlue1",fg="black")
 nameInput.place(relx=0.7,rely=0.4,anchor=CENTER)
-label2=Label(root,text="Activity:",font=("times",15),bg="CadetBlue1")
+label2=Label(root,text="Activity:",font=("times",15),bg="CadetBlue1",fg="black")
 label2.place(relx=0.3,rely=0.45,anchor=CENTER)
-activityInput=Entry(root,font=("times",12),bg="CadetBlue1")
+activityInput=Entry(root,font=("times",12),bg="CadetBlue1",fg="black")
 activityInput.place(relx=0.7,rely=0.45,anchor=CENTER)
-label3=Label(root,text="Time:",font=("times",15),bg="CadetBlue1")
+label3=Label(root,text="Time:",font=("times",15),bg="CadetBlue1",fg="black")
 label3.place(relx=0.3,rely=0.5,anchor=CENTER)
-timeInput=Entry(root,font=("times",12),bg="CadetBlue1")
+timeInput=Entry(root,font=("times",12),bg="CadetBlue1",fg="black")
 timeInput.place(relx=0.7,rely=0.5,anchor=CENTER)
+table=ttk.Treeview(root,columns=('name','activity','time'),show='headings')
+table.heading('name',text="Name")
+table.heading('activity',text="Activity")
+table.heading('time',text="Time")
+table.place(relx=0.5,rely=0.8,anchor=CENTER)
 def Add():
     global nameList
     global activityList
@@ -55,7 +61,15 @@ def Add():
     pickle.dump(timeList,open("time.dat","wb"))
     print("Time:")
     print(timeList)
+    name=val1
+    activity=val2
+    time=val3
+    data=(name,activity,time)
+    table.insert(parent="", index=END,values=data)
 def Clear():
+    def clearTable():
+        for thing in table.get_children():
+            table.delete(thing)
     nameList.clear()
     activityList.clear()
     timeList.clear()
@@ -68,14 +82,20 @@ def Clear():
     print(activityList)
     print("Time:")
     print(timeList)
+    clearTable()
 def Show():
-    labelName["text"]=str(nameList)
-    labelActivity["text"]=str(activityList)
-    labelTime["text"]=str(timeList)
-btnAdd=Button(root,text="Save",command=Add,font=("times",12),fg="medium spring green")
-btnAdd.place(relx=0.2,rely=0.6,anchor=CENTER)
+    print(len(nameList))
+    for i in range(len(nameList)):
+        name=nameList[i]
+        activity=activityList[i]
+        time=timeList[i]
+        data=(name,activity,time)
+        table.insert(parent="", index=END,values=data)
+    btnShow.destroy()
+btnAdd=Button(root,text="Add",command=Add,font=("times",12),fg="medium spring green")
+btnAdd.place(relx=0.3,rely=0.6,anchor=CENTER)
 btnClear=Button(root,text="Clear",command=Clear,font=("times",12),fg="medium spring green")
-btnClear.place(relx=0.5,rely=0.6,anchor=CENTER)
+btnClear.place(relx=0.7,rely=0.6,anchor=CENTER)
 btnShow=Button(root,text="Show",command=Show,font=("times",12),fg="medium spring green")
-btnShow.place(relx=0.8,rely=0.6,anchor=CENTER)
+btnShow.place(relx=0.5,rely=0.6,anchor=CENTER)
 root.mainloop()
--- README.md
+++ README.md
@@ -5,11 +5,3 @@ and list activities you have
 done during the day. It asks 
 for your name, the time, and 
 the activity.
-
-_Quick note_: To use this, you 
-first have to change the lists 
-from loading pickled files to 
-regular lists, load them once, 
-make an item in the list, and 
-then change it back. Otherwise 
-it won't have a file to go to.
