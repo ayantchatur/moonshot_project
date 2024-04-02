10f9274bac2c0146b1605aa8e6197d141422e007
ayantc <ayantchatur@gmail.com>
2024-04-02 16:23:38 -0700

    Adding style to the table This commit is to make the table slightly easier to look at!

--- MoonshotProject.py
+++ MoonshotProject.py
@@ -4,12 +4,13 @@ from PIL import Image, ImageTk
 import pickle
 import os
 from random import choice
+count=0
 def resized_label(filename="", width=215, height=215, relx=.5, rely=.25, anchor=CENTER):
     global root
     image = Image.open(filename)
     resize_image = image.resize((width, height))
     img = ImageTk.PhotoImage(resize_image)
-    label1 = Label(root, image=img)
+    label1 = Label(master=root, image=img,bg="CadetBlue1")
     label1.image = img
     label1.pack()
     label1.place(relx=relx, rely=rely, anchor=anchor)
@@ -19,7 +20,7 @@ activityList=pickle.load(open("activity.dat","rb"))
 timeList=pickle.load(open("time.dat","rb"))
 os.system("clear")
 root=Tk()
-root.geometry("700x800")
+root.geometry("700x950")
 root.title("Activity List")
 root.config(bg="CadetBlue1")
 checklist=resized_label("checklist.png",215,215,0.5,0.23)
@@ -37,15 +38,22 @@ label3=Label(root,text="Time:",font=("times",15),bg="CadetBlue1",fg="black")
 label3.place(relx=0.3,rely=0.5,anchor=CENTER)
 timeInput=Entry(root,font=("times",12),bg="CadetBlue1",fg="black")
 timeInput.place(relx=0.7,rely=0.5,anchor=CENTER)
+style=ttk.Style()
+style.theme_use('clam')
+style.configure("Treeview",bg="SkyBlue1",fg="black",rowheight=30,fieldbackground="SkyBlue1")
+style.map("Treeview",background=[('selected','aquamarine')],foreground=[('selected','black')])
 table=ttk.Treeview(root,columns=('name','activity','time'),show='headings')
 table.heading('name',text="Name")
 table.heading('activity',text="Activity")
 table.heading('time',text="Time")
 table.place(relx=0.5,rely=0.8,anchor=CENTER)
+table.tag_configure(tagname='odd',background="white")
+table.tag_configure(tagname='even ',background="SkyBlue1")
 def Add():
     global nameList
     global activityList
     global timeList
+    global count
     val1=nameInput.get()
     val2=activityInput.get()
     val3=timeInput.get()
@@ -65,8 +73,10 @@ def Add():
     activity=val2
     time=val3
     data=(name,activity,time)
-    table.insert(parent="", index=END,values=data)
+    table.insert(parent="", index=END,values=data,tags=('even'))
+    count=count+1
 def Clear():
+    global count
     def clearTable():
         for thing in table.get_children():
             table.delete(thing)
@@ -83,19 +93,21 @@ def Clear():
     print("Time:")
     print(timeList)
     clearTable()
+    count=0
 def Show():
-    print(len(nameList))
+    global count
     for i in range(len(nameList)):
         name=nameList[i]
         activity=activityList[i]
         time=timeList[i]
         data=(name,activity,time)
-        table.insert(parent="", index=END,values=data)
+        table.insert(parent="", index=END,values=data,tags=('even',))
+        count=count+1
     btnShow.destroy()
 btnAdd=Button(root,text="Add",command=Add,font=("times",12),fg="medium spring green")
 btnAdd.place(relx=0.3,rely=0.6,anchor=CENTER)
 btnClear=Button(root,text="Clear",command=Clear,font=("times",12),fg="medium spring green")
 btnClear.place(relx=0.7,rely=0.6,anchor=CENTER)
 btnShow=Button(root,text="Show",command=Show,font=("times",12),fg="medium spring green")
-btnShow.place(relx=0.5,rely=0.6,anchor=CENTER)
+btnShow.place(relx=0.5,rely=0.6,anchor=CENTER) 
 root.mainloop()
